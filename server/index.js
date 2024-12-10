3375495d0a891ca64c46475ae6d61849778c24f0
איילה כהן <131193595+LielAyala@users.noreply.github.com>
2024-12-11 00:42:12 +0200

    First commit I uploaded base files

---
+++ server/index.js
@@ -0,0 +1,18 @@
+/* ייבוא של Express*/
+const express = require('express');
+
+/* יצירת אפליקציה ב-Express*/
+const app = express();
+
+/* הגדרת הפורט*/
+const PORT = 3001;
+
+/* יצירת נתיב בסיסי לבדיקה*/
+app.get('/', (req, res) => {
+    res.send('השרת עובד כמו שצריך! 🚀');
+});
+
+/*האזנה לפורט*/
+app.listen(PORT, () => {
+    console.log(`השרת מאזין בפורט ${PORT}`);
+});
